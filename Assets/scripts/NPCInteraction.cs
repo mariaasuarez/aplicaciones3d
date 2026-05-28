b2cd7b16e95654217a9cac517a82b8f38da0be30
Andres Edilson Briceño Monroy <est.andrese.briceno@unimilitar.edu.co>
2026-05-28 17:44:37 -0500

    Dialogo 1

--- Assets/scripts/NPCInteraction.cs
+++ Assets/scripts/NPCInteraction.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class NPCInteraction : MonoBehaviour
 {
@@ -14,7 +15,7 @@ public class NPCInteraction : MonoBehaviour
     public GameObject dialoguePanel;
     public TMP_Text dialogueText;
 
-    [Header("Botón continuar")]
+    [Header("Botón continuar visual")]
     public Button continueButton;
 
     [Header("Opciones")]
@@ -38,30 +39,61 @@ public class NPCInteraction : MonoBehaviour
     public string response2;
     public string response3;
 
-    private bool playerInRange = false;
-    private int dialogueState = 0;
-    private int playerCollidersInside = 0;
+    [Header("Botones VR")]
+    public InputActionReference interactAction;
+    public InputActionReference continueAction;
+    public InputActionReference option1Action;
+    public InputActionReference option2Action;
+    public InputActionReference option3Action;
 
-    void Start()
+    private bool playerInRange = false;
+    private int playerCollidersInside = 0;
+    private int dialogueState = 0;
+    private int interactionCount = 0;
+
+    private void OnEnable()
+    {
+        ActivarAccion(interactAction, OnInteractVR);
+        ActivarAccion(continueAction, OnContinueVR);
+        ActivarAccion(option1Action, OnOption1VR);
+        ActivarAccion(option2Action, OnOption2VR);
+        ActivarAccion(option3Action, OnOption3VR);
+    }
+
+    private void OnDisable()
+    {
+        DesactivarAccion(interactAction, OnInteractVR);
+        DesactivarAccion(continueAction, OnContinueVR);
+        DesactivarAccion(option1Action, OnOption1VR);
+        DesactivarAccion(option2Action, OnOption2VR);
+        DesactivarAccion(option3Action, OnOption3VR);
+    }
+
+    private void Start()
     {
         canvasNPC.SetActive(false);
         interactButtonObject.SetActive(false);
         dialoguePanel.SetActive(false);
+        continueButton.gameObject.SetActive(false);
         HideOptions();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag("Player")) return;
+        if (!other.CompareTag("Player") && !other.transform.root.CompareTag("Player")) return;
 
         playerCollidersInside++;
         playerInRange = true;
-        interactButtonObject.SetActive(true);
+
+        if (dialogueState == 0)
+            interactButtonObject.SetActive(true);
+
+        Debug.Log("Jugador cerca del NPC");
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (!other.CompareTag("Player")) return;
+        if (!other.CompareTag("Player") && !other.transform.root.CompareTag("Player")) return;
 
         playerCollidersInside--;
 
@@ -75,56 +107,94 @@ public class NPCInteraction : MonoBehaviour
 
     public void Interact()
     {
-        Debug.Log("Interactuando con: " + gameObject.name);
-
         if (!playerInRange) return;
+
+        interactionCount++;
 
         canvasNPC.SetActive(true);
         interactButtonObject.SetActive(false);
         dialoguePanel.SetActive(true);
-
-        dialogueText.text = initialDialogue;
-
         HideOptions();
-        continueButton.gameObject.SetActive(true);
 
-        dialogueState = 1;
+        if (interactionCount < 3)
+        {
+            dialogueText.text = initialDialogue;
+            continueButton.gameObject.SetActive(true);
+            dialogueState = 1;
+
+            Debug.Log("Diálogo normal iniciado. Interacción: " + interactionCount);
+        }
+        else
+        {
+            dialogueText.text = finalDialogue;
+            continueButton.gameObject.SetActive(true);
+            dialogueState = 4;
+
+            Debug.Log("Tercera interacción: solo diálogo final");
+        }
     }
 
     public void ContinueDialogue()
     {
+        Debug.Log("BOTÓN CONTINUAR OPRIMIDO");
+
+        if (!playerInRange) return;
+        if (!canvasNPC.activeSelf) return;
+
         if (dialogueState == 1)
         {
             dialogueText.text = "";
             continueButton.gameObject.SetActive(false);
             ShowOptions();
             dialogueState = 2;
+
+            Debug.Log("Aparecieron las opciones");
         }
         else if (dialogueState == 3)
         {
             dialogueText.text = finalDialogue;
             continueButton.gameObject.SetActive(true);
             dialogueState = 4;
+
+            Debug.Log("Mostrando diálogo final");
         }
         else if (dialogueState == 4)
         {
             CloseDialogue();
+
+            Debug.Log("Diálogo cerrado");
         }
     }
 
     public void ChooseOption(int option)
     {
+        if (!playerInRange) return;
+        if (!canvasNPC.activeSelf) return;
+        if (dialogueState != 2) return;
+
         HideOptions();
 
-        if (option == 1) dialogueText.text = response1;
-        if (option == 2) dialogueText.text = response2;
-        if (option == 3) dialogueText.text = response3;
+        if (option == 1)
+        {
+            dialogueText.text = response1;
+            Debug.Log("OPCIÓN 1 OPRIMIDA");
+        }
+        else if (option == 2)
+        {
+            dialogueText.text = response2;
+            Debug.Log("OPCIÓN 2 OPRIMIDA");
+        }
+        else if (option == 3)
+        {
+            dialogueText.text = response3;
+            Debug.Log("OPCIÓN 3 OPRIMIDA");
+        }
 
         continueButton.gameObject.SetActive(true);
         dialogueState = 3;
     }
 
-    void ShowOptions()
+    private void ShowOptions()
     {
         optionButton1.SetActive(true);
         optionButton2.SetActive(true);
@@ -135,23 +205,67 @@ public class NPCInteraction : MonoBehaviour
         optionText3.text = option3;
     }
 
-    void HideOptions()
+    private void HideOptions()
     {
         optionButton1.SetActive(false);
         optionButton2.SetActive(false);
         optionButton3.SetActive(false);
     }
 
-    void CloseDialogue()
+    private void CloseDialogue()
     {
         canvasNPC.SetActive(false);
         dialoguePanel.SetActive(false);
+        continueButton.gameObject.SetActive(false);
         HideOptions();
+
         dialogueState = 0;
 
         if (playerInRange)
             interactButtonObject.SetActive(true);
         else
             interactButtonObject.SetActive(false);
+    }
+
+    private void OnInteractVR(InputAction.CallbackContext context)
+    {
+        Debug.Log("BOTÓN INTERACT OPRIMIDO");
+        Interact();
+    }
+
+    private void OnContinueVR(InputAction.CallbackContext context)
+    {
+        Debug.Log("INPUT CONTINUE DETECTADO");
+        ContinueDialogue();
+    }
+
+    private void OnOption1VR(InputAction.CallbackContext context)
+    {
+        ChooseOption(1);
+    }
+
+    private void OnOption2VR(InputAction.CallbackContext context)
+    {
+        ChooseOption(2);
+    }
+
+    private void OnOption3VR(InputAction.CallbackContext context)
+    {
+        ChooseOption(3);
+    }
+
+    private void ActivarAccion(InputActionReference accion, System.Action<InputAction.CallbackContext> metodo)
+    {
+        if (accion == null) return;
+
+        accion.action.Enable();
+        accion.action.performed += metodo;
+    }
+
+    private void DesactivarAccion(InputActionReference accion, System.Action<InputAction.CallbackContext> metodo)
+    {
+        if (accion == null) return;
+
+        accion.action.performed -= metodo;
     }
 }
